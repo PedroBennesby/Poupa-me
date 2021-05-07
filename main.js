2b0a9a8bf093591c8a288ec02fdbe82c42779cb8
Pedro Bennesby <pedrobennesby@gmail.com>
2021-05-06 22:34:32 -0300

    adicionado calculo inss

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-function soma() {
-  let a = document.getElementById("valor1").value;
-  let b = document.getElementById("valor2").value;
+function calcSalario() {
+  let salbruto = document.getElementById("salario-bruto").value;
 
-  let valoresInteiros1 = parseInt(a);
-  let valoresInteiros2 = parseInt(b);
+  let valorInteiro = parseFloat(salbruto);
 
-  let somaTotal = valoresInteiros1 + valoresInteiros2;
+  let aliVarInss = 0;
+  let parcDeduzirinss = 0;
+  let descInss = 0;
+  let salLiquido = 0;
 
-  document.getElementById("resultado").textContent = somaTotal;
+  if (valorInteiro <= 1100) {
+    aliVarInss = 7.5;
+  } else if (valorInteiro > 1100 && valorInteiro <= 2203.48) {
+    aliVarInss = 9;
+    parcDeduzirinss = 16.5;
+  } else if (valorInteiro > 2203.48 && valorInteiro <= 3305.22) {
+    aliVarInss = 12;
+    parcDeduzirinss = 82.61;
+  } else if (valorInteiro > 3305.22 && valorInteiro <= 6433.47) {
+    aliVarInss = 14;
+    parcDeduzirinss = 148.72;
+  } else {
+    aliVarInss = 751.99;
+  }
+
+  let calcBaseIrrf = valorInteiro - aliVarInss;
+
+  if (valorInteiro <= 6433.47) {
+    descInss = (valorInteiro / 100) * aliVarInss - parcDeduzirinss;
+    salLiquido = valorInteiro - descInss;
+  } else {
+    salLiquido = valorInteiro - aliVarInss;
+  }
+
+  document.getElementById("resultado").textContent = salLiquido;
 }
