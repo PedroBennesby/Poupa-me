48bfa40fec15ea988b748e2372f61f46c91a6487
Pedro Bennesby <pedrobennesby@gmail.com>
2021-05-19 18:15:54 -0300

    Alteração script

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function calcSalario() {
-  let salbruto = document.getElementById("salario-bruto").value;
+  let salbruto = document.getElementById('salario-bruto').value;
 
   let valorInteiro = parseFloat(salbruto);
 
   let aliVarInss = 0;
   let parcDeduzirinss = 0;
   let descInss = 0;
-  let salLiquido = 0;
+  let salPosInss = 0;
 
   if (valorInteiro <= 1100) {
     aliVarInss = 7.5;
@@ -23,14 +23,36 @@ function calcSalario() {
     aliVarInss = 751.99;
   }
 
-  let calcBaseIrrf = valorInteiro - aliVarInss;
+  let aliVarIrrf = 0;
+  let parcDeduzirIrrf = 0;
+  let salLiquido = 0;
+  let calcIrrf = 0;
 
   if (valorInteiro <= 6433.47) {
     descInss = (valorInteiro / 100) * aliVarInss - parcDeduzirinss;
-    salLiquido = valorInteiro - descInss;
+    salPosInss = valorInteiro - descInss;
   } else {
-    salLiquido = valorInteiro - aliVarInss;
+    salPosInss = valorInteiro - aliVarInss;
   }
 
-  document.getElementById("resultado").textContent = salLiquido;
+  if (salPosInss <= 1903.98) {
+    aliVarIrrf = 0;
+  } else if (salPosInss >= 1903.99 && salPosInss <= 2826.65) {
+    aliVarIrrf = 7.5;
+    parcDeduzirIrrf = 142.8;
+  } else if (salPosInss >= 2826.66 && salPosInss <= 3751.05) {
+    aliVarIrff = 15;
+    parcDeduzirIrrf = 354.8;
+  } else if (salPosInss >= 3751.06 && salPosInss <= 4664.68) {
+    aliVarIrrf = 22.5;
+    parcDeduzirIrrf = 636.13;
+  } else {
+    aliVarIrrf = 27.5;
+    parcDeduzirIrrf = 869.36;
+  }
+
+  calcIrrf = (salPosInss / 100) * aliVarIrrf - parcDeduzirIrrf;
+  salLiquido = salPosInss - calcIrrf;
+
+  document.getElementById('resultado').textContent = salLiquido;
 }
